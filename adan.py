062d4cd5de08c79fc89bec6ad814e1087ec34092
Alexwell <935562189@qq.com>
2023-02-07 08:11:34 +0100

    Update adan.py

--- adan.py
+++ adan.py
@@ -358,7 +358,7 @@ def _fused_adan(
         p_data_fp32 = param.data.float()
         out_p = param.data
         grad = grads[i]
-        grad_copy = grad.clone().mul_(clip_global_grad_norm)
+        # grad_copy = grad.clone().mul_(clip_global_grad_norm)
         exp_avg = exp_avgs[i]
         exp_avg_sq = exp_avg_sqs[i]
         exp_avg_diff = exp_avg_diffs[i]
@@ -385,4 +385,4 @@ def _fused_adan(
             no_prox,
             clip_global_grad_norm,
         )
-    neg_grad.zero_().add_(grad_copy, alpha=-1.0)
+    neg_grad.zero_().add_(grad.mul_(clip_global_grad_norm), alpha=-1.0)
