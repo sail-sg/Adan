0150025ea4e7d4f4dcbfdfd93f91e49bf987a621
Alexwell <935562189@qq.com>
2023-02-07 09:22:44 +0100

    Update fused_adan_kernel.cu

--- fused_adan/fused_adan_kernel.cu
+++ fused_adan/fused_adan_kernel.cu
@@ -90,115 +90,60 @@ __global__ void adan_cuda_kernel<float, float>(
         float4 new_exp_avg_sq4;
         float4 new_exp_avg_diff4;
 
-        // float scaled_grad1 = g4.x * clip_global_grad_norm;
-        // float scaled_grad2 = g4.y * clip_global_grad_norm;
-        // float scaled_grad3 = g4.z * clip_global_grad_norm;
-        // float scaled_grad4 = g4.w * clip_global_grad_norm;
+        float scaled_grad1 = g4.x * clip_global_grad_norm;
+        float scaled_grad2 = g4.y * clip_global_grad_norm;
+        float scaled_grad3 = g4.z * clip_global_grad_norm;
+        float scaled_grad4 = g4.w * clip_global_grad_norm;
 
-        float scaled_grad1 = __fmul_rn(g4.x, clip_global_grad_norm);
-        float scaled_grad2 = __fmul_rn(g4.y, clip_global_grad_norm);
-        float scaled_grad3 = __fmul_rn(g4.z, clip_global_grad_norm);
-        float scaled_grad4 = __fmul_rn(g4.w, clip_global_grad_norm);
+        float diff1 = scaled_grad1 + neg_grad4.x;
+        float diff2 = scaled_grad2 + neg_grad4.y;
+        float diff3 = scaled_grad3 + neg_grad4.z;
+        float diff4 = scaled_grad4 + neg_grad4.w;
 
-        // float diff1 = scaled_grad1 + neg_grad4.x;
-        // float diff2 = scaled_grad2 + neg_grad4.y;
-        // float diff3 = scaled_grad3 + neg_grad4.z;
-        // float diff4 = scaled_grad4 + neg_grad4.w;
+        float update1 = scaled_grad1 + b2 * diff1;
+        float update2 = scaled_grad2 + b2 * diff2;
+        float update3 = scaled_grad3 + b2 * diff3;
+        float update4 = scaled_grad4 + b2 * diff4;
 
-        float diff1 = __fadd_rn(scaled_grad1, neg_grad4.x);
-        float diff2 = __fadd_rn(scaled_grad2, neg_grad4.y);
-        float diff3 = __fadd_rn(scaled_grad3, neg_grad4.z);
-        float diff4 = __fadd_rn(scaled_grad4, neg_grad4.w);
+        new_exp_avg4.x = b1 * exp_avg4.x + (1 - b1) * scaled_grad1;
+        new_exp_avg4.y = b1 * exp_avg4.y + (1 - b1) * scaled_grad2;
+        new_exp_avg4.z = b1 * exp_avg4.z + (1 - b1) * scaled_grad3;
+        new_exp_avg4.w = b1 * exp_avg4.w + (1 - b1) * scaled_grad4;
 
-        // float update1 = scaled_grad1 + b2 * diff1;
-        // float update2 = scaled_grad2 + b2 * diff2;
-        // float update3 = scaled_grad3 + b2 * diff3;
-        // float update4 = scaled_grad4 + b2 * diff4;
+        new_exp_avg_sq4.x = b3 * exp_avg_sq4.x + (1 - b3) * update1 * update1;
+        new_exp_avg_sq4.y = b3 * exp_avg_sq4.y + (1 - b3) * update2 * update2;
+        new_exp_avg_sq4.z = b3 * exp_avg_sq4.z + (1 - b3) * update3 * update3;
+        new_exp_avg_sq4.w = b3 * exp_avg_sq4.w + (1 - b3) * update4 * update4;
 
-        float update1 = __fmaf_rn(b2, diff1, scaled_grad1);
-        float update2 = __fmaf_rn(b2, diff2, scaled_grad2);
-        float update3 = __fmaf_rn(b2, diff3, scaled_grad3);
-        float update4 = __fmaf_rn(b2, diff4, scaled_grad4);
-
-        // new_exp_avg4.x = b1 * exp_avg4.x + (1 - b1) * scaled_grad1;
-        // new_exp_avg4.y = b1 * exp_avg4.y + (1 - b1) * scaled_grad2;
-        // new_exp_avg4.z = b1 * exp_avg4.z + (1 - b1) * scaled_grad3;
-        // new_exp_avg4.w = b1 * exp_avg4.w + (1 - b1) * scaled_grad4;
-
-        float one_minus_b1 = 1 - b1;
-        float one_minus_b2 = 1 - b2;
-        float one_minus_b3 = 1 - b3;
-
-        new_exp_avg4.x = __fmaf_rn(b1, scaled_grad1, __fmul_rn(exp_avg4.x, one_minus_b1));
-        new_exp_avg4.y = __fmaf_rn(b1, scaled_grad2, __fmul_rn(exp_avg4.y, one_minus_b1));
-        new_exp_avg4.z = __fmaf_rn(b1, scaled_grad3, __fmul_rn(exp_avg4.z, one_minus_b1));
-        new_exp_avg4.w = __fmaf_rn(b1, scaled_grad4, __fmul_rn(exp_avg4.w, one_minus_b1));
-
-
-        // new_exp_avg_sq4.x = b3 * exp_avg_sq4.x + (1 - b3) * update1 * update1;
-        // new_exp_avg_sq4.y = b3 * exp_avg_sq4.y + (1 - b3) * update2 * update2;
-        // new_exp_avg_sq4.z = b3 * exp_avg_sq4.z + (1 - b3) * update3 * update3;
-        // new_exp_avg_sq4.w = b3 * exp_avg_sq4.w + (1 - b3) * update4 * update4;
-
-        new_exp_avg_sq4.x = __fmaf_rn(b3, __powf(update1, 2), __fmul_rn(exp_avg_sq4.x, one_minus_b3));
-        new_exp_avg_sq4.y = __fmaf_rn(b3, __powf(update2, 2), __fmul_rn(exp_avg_sq4.y, one_minus_b3));
-        new_exp_avg_sq4.z = __fmaf_rn(b3, __powf(update3, 2), __fmul_rn(exp_avg_sq4.z, one_minus_b3));
-        new_exp_avg_sq4.w = __fmaf_rn(b3, __powf(update4, 2), __fmul_rn(exp_avg_sq4.w, one_minus_b3));
-
-        // new_exp_avg_diff4.x = b2 * exp_avg_diff4.x + (1 - b2) * diff1;
-        // new_exp_avg_diff4.y = b2 * exp_avg_diff4.y + (1 - b2) * diff2;
-        // new_exp_avg_diff4.z = b2 * exp_avg_diff4.z + (1 - b2) * diff3;
-        // new_exp_avg_diff4.w = b2 * exp_avg_diff4.w + (1 - b2) * diff4;
-
-        new_exp_avg_diff4.x = __fmaf_rn(b2, diff1, __fmul_rn(exp_avg_diff4.x, one_minus_b2));
-        new_exp_avg_diff4.y = __fmaf_rn(b2, diff2, __fmul_rn(exp_avg_diff4.y, one_minus_b2));
-        new_exp_avg_diff4.z = __fmaf_rn(b2, diff3, __fmul_rn(exp_avg_diff4.z, one_minus_b2));
-        new_exp_avg_diff4.w = __fmaf_rn(b2, diff4, __fmul_rn(exp_avg_diff4.w, one_minus_b2));
+        new_exp_avg_diff4.x = b2 * exp_avg_diff4.x + (1 - b2) * diff1;
+        new_exp_avg_diff4.y = b2 * exp_avg_diff4.y + (1 - b2) * diff2;
+        new_exp_avg_diff4.z = b2 * exp_avg_diff4.z + (1 - b2) * diff3;
+        new_exp_avg_diff4.w = b2 * exp_avg_diff4.w + (1 - b2) * diff4;
 
         float4 denom4;
+        denom4.x = sqrt(new_exp_avg_sq4.x - new_exp_avg_diff4.x * new_exp_avg_diff4.x / b2) + eps;
+        denom4.y = sqrt(new_exp_avg_sq4.y - new_exp_avg_diff4.y * new_exp_avg_diff4.y / b2) + eps;
+        denom4.z = sqrt(new_exp_avg_sq4.z - new_exp_avg_diff4.z * new_exp_avg_diff4.z / b2) + eps;
+        denom4.w = sqrt(new_exp_avg_sq4.w - new_exp_avg_diff4.w * new_exp_avg_diff4.w / b2) + eps;
 
-        // denom4.x = sqrt(new_exp_avg_sq4.x - new_exp_avg_diff4.x * new_exp_avg_diff4.x / b2) + eps;
-        // denom4.y = sqrt(new_exp_avg_sq4.y - new_exp_avg_diff4.y * new_exp_avg_diff4.y / b2) + eps;
-        // denom4.z = sqrt(new_exp_avg_sq4.z - new_exp_avg_diff4.z * new_exp_avg_diff4.z / b2) + eps;
-        // denom4.w = sqrt(new_exp_avg_sq4.w - new_exp_avg_diff4.w * new_exp_avg_diff4.w / b2) + eps;
-
-        // use __fadd_rn, __fsqrt_rn, __fdiv_rn, __fmul_rn, __fmaf_rn
-        denom4.x = __fadd_rn(__fsqrt_rn(__fsub_rn(new_exp_avg_sq4.x, __fdiv_rn(__fmul_rn(new_exp_avg_diff4.x, new_exp_avg_diff4.x), b2))), eps);
-        denom4.y = __fadd_rn(__fsqrt_rn(__fsub_rn(new_exp_avg_sq4.y, __fdiv_rn(__fmul_rn(new_exp_avg_diff4.y, new_exp_avg_diff4.y), b2))), eps);
-        denom4.z = __fadd_rn(__fsqrt_rn(__fsub_rn(new_exp_avg_sq4.z, __fdiv_rn(__fmul_rn(new_exp_avg_diff4.z, new_exp_avg_diff4.z), b2))), eps);
-        denom4.w = __fadd_rn(__fsqrt_rn(__fsub_rn(new_exp_avg_sq4.w, __fdiv_rn(__fmul_rn(new_exp_avg_diff4.w, new_exp_avg_diff4.w), b2))), eps);
-
-        // update1 = (new_exp_avg4.x / bias_correction1 + b2 * new_exp_avg_diff4.x / bias_correction2) / denom4.x;
-        // update2 = (new_exp_avg4.y / bias_correction1 + b2 * new_exp_avg_diff4.y / bias_correction2) / denom4.y;
-        // update3 = (new_exp_avg4.z / bias_correction1 + b2 * new_exp_avg_diff4.z / bias_correction2) / denom4.z;
-        // update4 = (new_exp_avg4.w / bias_correction1 + b2 * new_exp_avg_diff4.w / bias_correction2) / denom4.w;
-
-        // use __fadd_rn, __fsqrt_rn, __fdiv_rn, __fmul_rn, __fmaf_rn
-        update1 = __fdiv_rn(__fadd_rn(__fdiv_rn(new_exp_avg4.x, bias_correction1), __fdiv_rn(__fmul_rn(b2, new_exp_avg_diff4.x), bias_correction2)), denom4.x);
-        update2 = __fdiv_rn(__fadd_rn(__fdiv_rn(new_exp_avg4.y, bias_correction1), __fdiv_rn(__fmul_rn(b2, new_exp_avg_diff4.y), bias_correction2)), denom4.y);
-        update3 = __fdiv_rn(__fadd_rn(__fdiv_rn(new_exp_avg4.z, bias_correction1), __fdiv_rn(__fmul_rn(b2, new_exp_avg_diff4.z), bias_correction2)), denom4.z);
-        update4 = __fdiv_rn(__fadd_rn(__fdiv_rn(new_exp_avg4.w, bias_correction1), __fdiv_rn(__fmul_rn(b2, new_exp_avg_diff4.w), bias_correction2)), denom4.w);
+        // update = (exp_avg[global_id] / bias_correction1 + b2 * exp_avg_diff[global_id] / bias_correction2) / denom;
+        update1 = (new_exp_avg4.x / bias_correction1 + b2 * new_exp_avg_diff4.x / bias_correction2) / denom4.x;
+        update2 = (new_exp_avg4.y / bias_correction1 + b2 * new_exp_avg_diff4.y / bias_correction2) / denom4.y;
+        update3 = (new_exp_avg4.z / bias_correction1 + b2 * new_exp_avg_diff4.z / bias_correction2) / denom4.z;
+        update4 = (new_exp_avg4.w / bias_correction1 + b2 * new_exp_avg_diff4.w / bias_correction2) / denom4.w;
 
         if (no_prox){
-            // new_p4.x = p4.x * (1 - lr * decay) + update1 * (-lr);
-            // new_p4.y = p4.y * (1 - lr * decay) + update2 * (-lr);
-            // new_p4.z = p4.z * (1 - lr * decay) + update3 * (-lr);
-            // new_p4.w = p4.w * (1 - lr * decay) + update4 * (-lr);
-            // use __fadd_rn, __fsqrt_rn, __fdiv_rn, __fmul_rn, __fmaf_rn
-            new_p4.x = __fadd_rn(__fmul_rn(p4.x, __fsub_rn(1, __fmul_rn(lr, decay))), __fmul_rn(update1, __fsub_rn(0, lr)));
-            new_p4.y = __fadd_rn(__fmul_rn(p4.y, __fsub_rn(1, __fmul_rn(lr, decay))), __fmul_rn(update2, __fsub_rn(0, lr)));
-            new_p4.z = __fadd_rn(__fmul_rn(p4.z, __fsub_rn(1, __fmul_rn(lr, decay))), __fmul_rn(update3, __fsub_rn(0, lr)));
-            new_p4.w = __fadd_rn(__fmul_rn(p4.w, __fsub_rn(1, __fmul_rn(lr, decay))), __fmul_rn(update4, __fsub_rn(0, lr)));
+            // p[global_id] = p[global_id] * (1 - lr * decay) + update * (-lr);
+            new_p4.x = p4.x * (1 - lr * decay) + update1 * (-lr);
+            new_p4.y = p4.y * (1 - lr * decay) + update2 * (-lr);
+            new_p4.z = p4.z * (1 - lr * decay) + update3 * (-lr);
+            new_p4.w = p4.w * (1 - lr * decay) + update4 * (-lr);
         }else{
-            // new_p4.x = p4.x + update1 * (-lr) / (1 + lr * decay);
-            // new_p4.y = p4.y + update2 * (-lr) / (1 + lr * decay);
-            // new_p4.z = p4.z + update3 * (-lr) / (1 + lr * decay);
-            // new_p4.w = p4.w + update4 * (-lr) / (1 + lr * decay);
-            // use __fadd_rn, __fsqrt_rn, __fdiv_rn, __fmul_rn, __fmaf_rn
-            new_p4.x = __fadd_rn(p4.x, __fdiv_rn(__fmul_rn(update1, __fsub_rn(0, lr)), __fadd_rn(1, __fmul_rn(lr, decay))));
-            new_p4.y = __fadd_rn(p4.y, __fdiv_rn(__fmul_rn(update2, __fsub_rn(0, lr)), __fadd_rn(1, __fmul_rn(lr, decay))));
-            new_p4.z = __fadd_rn(p4.z, __fdiv_rn(__fmul_rn(update3, __fsub_rn(0, lr)), __fadd_rn(1, __fmul_rn(lr, decay))));
-            new_p4.w = __fadd_rn(p4.w, __fdiv_rn(__fmul_rn(update4, __fsub_rn(0, lr)), __fadd_rn(1, __fmul_rn(lr, decay))));
+            // p[global_id] = p[global_id] + update * (-lr) / (1 + lr * decay);
+            new_p4.x = p4.x + update1 * (-lr) / (1 + lr * decay);
+            new_p4.y = p4.y + update2 * (-lr) / (1 + lr * decay);
+            new_p4.z = p4.z + update3 * (-lr) / (1 + lr * decay);
+            new_p4.w = p4.w + update4 * (-lr) / (1 + lr * decay);
         }   
 
         p4_ptr[global_id] = new_p4;
