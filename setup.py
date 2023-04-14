cab672420e62a9a4bfefc4f778a4bda75bd054fc
janEbert <janpublicebert@posteo.net>
2023-04-14 08:11:33 +0200

    Allow unconditional CUDA build (#30)
    * Allow unconditional CUDA build
    
    Useful when compiling on a machine without CUDA, but deploying on a
    machine with CUDA.
    
    * Error out early with helpful messages
    
    * Increment version
    
    Due to the addition of fused Adan.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
+import os
 from setuptools import setup
+
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 from torch.cuda import is_available
 
@@ -7,10 +9,12 @@ cuda_extension = CUDAExtension(
             sources=['fused_adan/pybind_adan.cpp','./fused_adan/fused_adan_kernel.cu', './fused_adan/multi_tensor_adan_kernel.cu']
         )
 
+build_cuda_ext = is_available() or os.getenv('FORCE_CUDA', '0') == '1'
+
 setup(
     name='adan',
     python_requires='>=3.8',
-    version='0.0.1',
+    version='0.0.2',
     install_requires=['torch'],
     py_modules=['adan'],
     description=(
@@ -21,6 +25,6 @@ setup(
         'Xie, Xingyu and Zhou, Pan and Li, Huan and '
         'Lin, Zhouchen and Yan, Shuicheng'
     ),
-    ext_modules=[cuda_extension] if is_available() else [],
-    cmdclass={'build_ext': BuildExtension} if is_available() else {},
+    ext_modules=[cuda_extension] if build_cuda_ext else [],
+    cmdclass={'build_ext': BuildExtension} if build_cuda_ext else {},
 )
