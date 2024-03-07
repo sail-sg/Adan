7d23ededcffbc25ba0cbbef9f7721248620bea7c
Alexwell <935562189@qq.com>
2024-03-07 13:09:45 +0000

    Fix CUDAExtension bug in setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 from torch.cuda import is_available
 
+build_cuda_ext = is_available() or os.getenv('FORCE_CUDA', '0') == '1'
+
+cuda_extension = None
 if "--unfused" in sys.argv:
     print("Building unfused version of adan")
     sys.argv.remove("--unfused")
-else:
+elif build_cuda_ext:
     cuda_extension = CUDAExtension(
-            'fused_adan', 
-            sources=['fused_adan/pybind_adan.cpp','./fused_adan/fused_adan_kernel.cu', './fused_adan/multi_tensor_adan_kernel.cu']
-        )
-
-build_cuda_ext = is_available() or os.getenv('FORCE_CUDA', '0') == '1'
+        'fused_adan', 
+        sources=['fused_adan/pybind_adan.cpp', './fused_adan/fused_adan_kernel.cu', './fused_adan/multi_tensor_adan_kernel.cu']
+    )
 
 setup(
     name='adan',
@@ -30,6 +31,6 @@ setup(
         'Xie, Xingyu and Zhou, Pan and Li, Huan and '
         'Lin, Zhouchen and Yan, Shuicheng'
     ),
-    ext_modules=[cuda_extension] if build_cuda_ext else [],
+    ext_modules=[cuda_extension] if cuda_extension is not None else [],
     cmdclass={'build_ext': BuildExtension} if build_cuda_ext else {},
 )
